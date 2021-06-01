94ef704b893beb71e52c6700b1ffa355dc6932f4
Sumit Govil <sumg.work@gmail.com>
2021-06-01 16:02:01 +1000

    🔧 CONFIG: Webpack prod config for container and marketing

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,23 +3,22 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies, // to load only one copy for all modules in browser
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -3,23 +3,23 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies, // to load only one copy for all modules in browser
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
