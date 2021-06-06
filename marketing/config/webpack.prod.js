9a46e6ea882d82ce9a3bb63c7adf58867ae39614
Sumit Govil <sumg.work@gmail.com>
2021-06-06 19:24:28 +1000

    🔧 CONFIG: Marketing workflow

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -3,12 +3,11 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
