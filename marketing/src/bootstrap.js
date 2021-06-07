6e91835970c883d2f4948de11cf89e25abf89f7d
Sumit Govil <sumg.work@gmail.com>
2021-06-07 17:42:00 +1000

    ✨ NEW FEATURE: Navigation on container and sub-apps

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate); // whenever path changes, call onNavigate function
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log("Container navigates", nextPathname);
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
