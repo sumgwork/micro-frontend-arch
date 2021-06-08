46d1327024887cdd96fa197a27a424279daeb0cf
Sumit Govil <sumg.work@gmail.com>
2021-06-08 12:32:27 +1000

    ✨ NEW FEATURE: Authentication App

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,23 +1,27 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: "index.html",
+      index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: 'container',
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        auth: 'auth@http://localhost:8082/remoteEntry.js',
       },
-      shared: packageJson.dependencies, // to load only one copy for all modules in browser
+      shared: packageJson.dependencies,
     }),
   ],
 };
